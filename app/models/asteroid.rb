72fe4de13373369485b8407394fa1f714476f657
Ben <ben.c.hawker@gmail.com>
2015-09-03 17:05:20 +0100

    Delta complete

--- app/models/asteroid.rb
+++ app/models/asteroid.rb
@@ -1,2 +1,4 @@
 class Asteroid < ActiveRecord::Base
+	has_many :orbitings
+	has_many :planets, through: :orbitings
 end
--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,2 +1,6 @@
 class Planet < ActiveRecord::Base
+	belongs_to :sun
+	has_many :moons
+	has_many :orbitings
+	has_many :asteroids, through: :orbitings
 end
